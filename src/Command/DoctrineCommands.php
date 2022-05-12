eeb42d422c52cb6b6fc6f64874ea03a05520712d
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-12 11:13:05 +0200

    Doctrine cli working

--- src/Command/DoctrineCommands.php
+++ src/Command/DoctrineCommands.php
@@ -10,7 +10,13 @@ class DoctrineCommands
     public static function runCommand()
     {
         $entityManager = DoctrineService::getEntityManager();
-
-        return ConsoleRunner::createHelperSet($entityManager);
+        $helperSet = ConsoleRunner::createHelperSet($entityManager);
+        unset($_SERVER['argv'][1]);
+        foreach($_SERVER['argv'] as $index => $value) {
+            if($value === '--allow-root') {
+                unset($_SERVER['argv'][$index]);
+            }
+        }
+        ConsoleRunner::run($helperSet);
     }
 }
