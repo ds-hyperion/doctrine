5b93075250e5080dfdaa9f363c4a0b3e49c1f923
d4rk0s <gregory@d4rk0s.net>
2022-06-13 16:42:00 +0200

    Ajout du petit tweak pour mettre à null le parent dans la table post

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,6 +11,28 @@ class Plugin
     public const ADD_ENTITIES_FILTER = 'doctrine_entities';
     public const ADD_EVENT_FILTER = 'doctrine_events';
 
+    public static function onActivation()
+    {
+        global $wpdb;
+        $dbPrefix = getenv('DB_PREFIX') === false ? 'wp_' : getenv('DB_PREFIX');
+
+        $wpdb->query("ALTER TABLE ".$dbPrefix."posts modify post_parent bigint unsigned null");
+        $wpdb->query("CREATE TRIGGER triggerInsertZeroToNull BEFORE INSERT ON ".$dbPrefix."posts FOR EACH ROW IF NEW.post_parent = 0 THEN SET NEW.post_parent = null; END IF;");
+        $wpdb->query("CREATE TRIGGER triggerUpdateZeroToNull BEFORE UPDATE ON ".$dbPrefix."posts FOR EACH ROW IF NEW.post_parent = 0 THEN SET NEW.post_parent = null; END IF;");
+        $wpdb->query("UPDATE ".$dbPrefix."posts SET post_parent = null WHERE post_parent=0;");
+    }
+
+    public static function onDeactivation()
+    {
+        global $wpdb;
+        $dbPrefix = getenv('DB_PREFIX') === false ? 'wp_' : getenv('DB_PREFIX');
+
+        $wpdb->query("UPDATE ".$dbPrefix."posts SET post_parent = 0 WHERE post_parent IS NULL");
+        $wpdb->query("DROP TRIGGER triggerInsertZeroToNull");
+        $wpdb->query("DROP TRIGGER triggerUpdateZeroToNull");
+        $wpdb->query("ALTER TABLE ".$dbPrefix."posts modify post_parent bigint unsigned default 0 not null");
+    }
+
     public static function init()
     {
         add_filter(self::ADD_ENTITIES_FILTER, 'Hyperion\Doctrine\Plugin::addWordPressEntityPath');
